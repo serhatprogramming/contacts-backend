0dc11d5e2b4cbbbfafef2f04b1310f5b1d135383
serhatprogramming <serhatprogramming@gmail.com>
2023-06-14 18:51:12 -0400

    port

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ app.post("/api/contacts", (req, res) => {
 });
 
 // Start the server
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log("Server is running on port 3001");
 });
